2014828906db01eff7fe75380edae4d02481b9a6
Giacomo Alonzi <giacomo@mozestudio.com>
2020-03-10 12:31:10 +0100

    fix analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,7 +54,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId,
+        trackingId: "UA-56138240-7",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
         // Setting this parameter is optional
