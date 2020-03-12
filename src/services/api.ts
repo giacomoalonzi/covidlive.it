67ee7cab9ef961ab360be3b12d4d9fff9d495239
Giacomo Alonzi <giacomo@mozestudio.com>
2020-03-12 12:23:29 +0100

    Feature/now (#38)
    * add .env support

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,8 @@
 import axios from "axios"
 import { normalizeRegionData, normalizeNationalTrendData } from "./normalize"
 
-const baseURL = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json"
-
 const http = axios.create({
-  baseURL,
+  baseURL: process.env.GATSBY_API_URL,
   timeout: process.env.NODE_ENV !== "production" ? 100000 : 10000,
 })
 
