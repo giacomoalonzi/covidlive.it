9a38595e086a0cadc96c0486c95b83a59fc571c5
Giacomo Alonzi <giacomo@mozestudio.com>
2020-03-23 09:45:28 +0100

    update api endpoint

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,15 +1,43 @@
 import axios from "axios"
 import { normalizeRegionData, normalizeNationalTrendData } from "./normalize"
 
-const http = axios.create({
+const httpLocalData = axios.create({
   baseURL: process.env.GATSBY_API_URL,
   timeout: process.env.NODE_ENV !== "production" ? 100000 : 10000,
 })
 
+const httpWorldData = axios.create({
+  baseURL: process.env.GATSBY_API_WORLD_DATA_URL,
+  timeout: process.env.NODE_ENV !== "production" ? 100000 : 10000,
+  headers: { "Content-Type": "application/json" },
+})
+
 export const getRegionsData = () => {
-  return http.get("/dpc-covid19-ita-regioni.json").then(normalizeRegionData)
+  return httpLocalData.get("/dpc-covid19-ita-regioni.json").then(normalizeRegionData)
 }
 
 export const getNationalTrendData = () => {
-  return http.get("/dpc-covid19-ita-andamento-nazionale.json").then(normalizeNationalTrendData)
+  return httpLocalData.get("/dpc-covid19-ita-andamento-nazionale.json").then(normalizeNationalTrendData)
 }
+export const getWorldData = () => {
+  return httpWorldData
+    .post("/graphql", {
+      query: `
+      {
+        countries {
+          country
+          cases
+          todayCases
+          deaths
+          todayDeaths
+          recovered
+          active
+          critical
+          casesPerOneMillion
+        } 
+      }
+    `,
+    })
+    .then(console.log)
+}
+getWorldData()
